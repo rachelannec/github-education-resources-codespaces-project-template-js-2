561f5e608ac5bf7426d45b6e5e35bfaf2ac927c7
Rachel Anne Cilon <113764045+racilon@users.noreply.github.com>
2024-09-28 11:18:15 +0000

    Education update

---
+++ src/Components/Education.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section>
+      <div id="main" style={{ paddingTop: "3rem" }}>
+        <h2 style={{ textAlign: "center" }}>Education</h2>
+        <div style={{ maxWidth: "40%", alignSelf: "center" }}>
+          <ul>
+            <li>
+              <h3>Polytechnic University of the Philippines</h3>
+              <p>Bachelor of Science in Computer Science</p>
+              <p>2024 - Present</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
